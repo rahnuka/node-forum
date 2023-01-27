5740ac40f71b6c7b76755961f42664f1fbc3add4
Amirah Cazeneuve <amirahcazeneuve@outlook.com>
2023-01-27 13:14:38 +0000

    creating forum post page

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+// Entry Point of the API Server 
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 app.use(express.json())
 const bcrypt=require("bcrypt.js");
-
-const cors= require("cors")
-app.use(cors());
+var cors = require('cors')
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({
+    origin: '*',
+}))
 
 
 const mongoUrl=
@@ -45,10 +50,112 @@ app.post("/Signup", async (req, res) => {
     }
 });
 
+const test = [
+    {
+      author: "James",
+      title: "My Stuff",
+      posted: "01/01/2023",
+      likes: 5,
+      dislikes: 10,
+      content: "My post content",
+      replies: [
+        {
+          author: "James",
+          title: "My Stuff",
+          posted: "01/01/2023",
+          likes: 5,
+          dislikes: 10,
+          content: "My post content",
+          replies: [
+            {
+              author: "James",
+              title: "My Stuff",
+              posted: "01/01/2023",
+              likes: 5,
+              dislikes: 10,
+              content: "My post content",
+              replies: [
+                {
+                  author: "James",
+                  title: "My Stuff",
+                  posted: "01/01/2023",
+                  likes: 5,
+                  dislikes: 10,
+                  content: "My post content",
+                  replies: [
+                    {
+                      author: "James",
+                      title: "My Stuff",
+                      posted: "01/01/2023",
+                      likes: 5,
+                      dislikes: 10,
+                      content: "My post content",
+                      replies: [
+                        
+                      ]
+                    },
+                  ]
+                },
+              ]
+            },
+          ]
+        },
+        {
+          author: "James",
+          title: "My Stuff",
+          posted: "01/01/2023",
+          likes: 5,
+          dislikes: 10,
+          content: "My post content",
+          replies: [
+            
+          ]
+        },
+        {
+          author: "James",
+          title: "My Stuff",
+          posted: "01/01/2023",
+          likes: 5,
+          dislikes: 10,
+          content: "My post content",
+          replies: [
+            
+          ]
+        },
+      ]
+    },
+]
 
-app.listen(5000, () => {
-    console.log("Server Started");
-});
+app.get('/testdata', (req, res, next) => {
+    res.send(test);
+})
+
+app.post('/posts', (req, res, next) => {
+    if(req.body.title === undefined) return res.status(400).send({
+        message: 'Title not found'
+    });
+    if(req.body.author === undefined) return res.status(400).send({
+        message: 'Author not found'
+    });
+    if(req.body.content === undefined) return res.status(400).send({
+        message: 'Content not found'
+    });
+    test.push(req.body);
+    res.status(200).send({
+        message: 'success'
+    })
+})
+
+// Require the Routes API  
+// Create a Server and run it on the port 3000
+const server = app.listen(4000, function () {
+    let host = server.address().address
+    let port = server.address().port
+    console.log("Server Started")
+    // Starting the Server at the port 3000
+})
+
+
 
 // mongo user and password
 // user: rah
